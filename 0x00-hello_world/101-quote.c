6d65abff515d631779b9facbd8e8a25df7d60a3a
haggaingosamwape <ngosahaggai13@gmail.com>
2022-09-08 14:27:41 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <string.h>
 #include <unistd.h>
 /**
- *  * main - main block
- *   * Return: 1
- *    */
+ * main - main block
+ * Return: 1
+ */
 int main(void)
 {
-		int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-			write(2,
-						      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-						      	      s);
-				return (1);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
 }
