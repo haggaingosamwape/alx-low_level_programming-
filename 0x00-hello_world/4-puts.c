2ff78f7b401320e5dcd097bfffb7f01e1e00f121
haggaingosamwape <ngosahaggai13@gmail.com>
2022-09-08 13:29:20 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+Footer
