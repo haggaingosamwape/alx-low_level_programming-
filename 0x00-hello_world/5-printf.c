504774c5c6c53c891033ca5278a72acb4cc451a9
haggaingosamwape <ngosahaggai13@gmail.com>
2022-09-08 14:05:39 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - entry point
- *   * Return: 0
- *    */
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
-} 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
