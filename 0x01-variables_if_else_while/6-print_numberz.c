c5f6485cc06238438c37147eeb3fc7c796869185
haggaingosamwape <ngosahaggai13@gmail.com>
2022-09-09 14:01:00 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Description: Print the alphabet in reverse order in lowercase.
- *    * Return: 0
- *     */
+ * main - main block
+ * Description: Print the alphabet in reverse order in lowercase.
+ * Return: 0
+ */
 int main(void)
 {
-		char c;
+	char c;
 
-			for (c = 'z'; c >= 'a'; c--)
-					{
-								putchar(c);
-									}
-				putchar('\n');
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
