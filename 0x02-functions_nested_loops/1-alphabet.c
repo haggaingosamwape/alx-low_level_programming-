dac0dfb6a6594dfdf7134ca4af86835eab66c940
haggaingosamwape <ngosahaggai13@gmail.com>
2022-09-13 07:02:28 -0700

    haggai

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *   *
+ *    * Return: 0 (Sucees)
+ *     */
+int print_alphabet(void)
+{
+		char ch;
+
+			ch = 'a';
+
+				while (ch <= 'z')
+						{
+									_putchar(ch);
+											ch++;
+												}
+
+					_putchar('\n');
+						return (0);
+}
