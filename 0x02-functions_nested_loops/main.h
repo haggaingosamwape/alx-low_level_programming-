4a3ff30db98d47f412f5ab2b51e0d98f8b8f70e1
haggaingosamwape <ngosahaggai13@gmail.com>
2022-09-13 20:58:39 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,34 @@
-int _putchar(char c);
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
+/* _putchar.c */
+int _putchar(char);
+
+/*10 x alphabet */
 void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
+
 int _islower(int c);
-int _isalpha(int c);
+
+int _isalpha(int n);
+
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
 void jack_bauer(void);
+
 void times_table(void);
-int add(int, int);
+
+int add(int a, int b);
+
 void print_to_98(int n);
+
 void print_times_table(int n);
+
+#endif
